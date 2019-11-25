4747906cc958ab46de5e528841d542dac4cd0e9d
Juan Pablo Ortiz <ortizj@tenaska.com>
2019-11-25 03:48:05 -0600

    Commit parcial

---
+++ Data/GenericRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Data
+{
+    public class GenericRepository : IGenericRepository
+    {
+        private readonly DataContext _context;
+
+        public GenericRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public void Add<T>(T entity) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> Getuser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
